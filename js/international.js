16683b6b7afee4d71685b6f07eb5c6c269b290b4
Davide <info.davide99@gmail.com>
2022-02-09 11:35:29 +0100

    Traduzione funzionante, da completare

---
+++ js/international.js
@@ -0,0 +1,34 @@
+/*
+ *  Mr. Worldwide
+ */
+
+const supportedLocales = ["en", "it"];
+
+const translations = {
+    "en": {
+        "welcome": "Welcome to"
+    },
+    "it": {
+        "welcome": "Benvenuto su"
+    },
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    //Ottengo locales preferiti
+    const browserLocales = navigator.languages.map((locale) => locale.split("-")[0]);
+    //Trovo quelli comuni
+    const intersection = browserLocales.filter(value => supportedLocales .includes(value));
+
+    if (intersection.length > 0)
+        var currentLocale = intersection[0];
+    else
+        var currentLocale = "en";
+
+    document
+        .querySelectorAll("[data-i18n-key]")
+        .forEach((element) => {
+            const key = element.getAttribute("data-i18n-key");
+            const translation = translations[currentLocale][key];
+            element.innerText = translation;
+        });
+});
